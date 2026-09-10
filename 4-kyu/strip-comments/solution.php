bfff14db2821a89d9f9366dc822b881a0689acf9
Justino Contingo <protectedum@proton.me>
2026-09-10 08:42:10 +0100

    refactor: Organized the code into a function and improved variable names.

--- 4-kyu/strip-comments/solution.php
+++ 4-kyu/strip-comments/solution.php
@@ -1,28 +1,32 @@
 <?php
+function stripComments(string $str, array $markers): string
+{
+  for ($i = 0; $i < count($markers); $i++) {
+    $pos = strpos($str, $markers[$i]);
 
+    while ($pos !== false) {
+      $str_before = substr($str, 0, $pos);
+      $str_next = "";
+      $pos_n = strpos($str, "\n", $pos);
 
+      if ($pos_n !== false) {
+        $str_next = substr($str, $pos_n);
+      }
 
+      $str = rtrim($str_before, " ") . $str_next;
 
-
-$s = "apples, pears # and bananas\ngrapes\nbananas !apples \n# comment";
-$m = ['#', '!'];
-
-for ($i = 0; $i < count($m); $i++) {
-  $pos = strpos($s, $m[$i]);
-
-  while ($pos !== false) {
-    $sub_befor = substr($s, 0, $pos);
-    $sub_next = "";
-    $pos_n = strpos($s, "\n", $pos);
-
-    if ($pos_n !== false) {
-      $sub_next = substr($s, $pos_n);
+      $pos = strpos($str, $markers[$i]);
     }
-
-    $s = rtrim($sub_befor) . $sub_next;
-
-    $pos = strpos($s, $m[$i]);
   }
+
+  return $str;
 }
 
-echo $s . "\n";
+// test
+
+$str = "apples, pears # and bananas\ngrapes\nbananas !apples \n# comment";
+$markers = ['#', '!'];
+
+$result = stripComments($str, $markers);
+
+echo $result . "\n";
